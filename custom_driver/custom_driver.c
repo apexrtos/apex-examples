5dd187b90e90958462731dea35971d1b107ad9fa
Patrick Oppenlander <patrick.oppenlander@gmail.com>
2019-08-02 16:11:30 +1000

    apex: update

--- custom_driver/custom_driver.c
+++ custom_driver/custom_driver.c
@@ -5,7 +5,6 @@
 #include <debug.h>
 #include <device.h>
 #include <errno.h>
-#include <fs/file.h>
 #include <fs/util.h>
 #include <string.h>
 #include <sys/param.h>
@@ -17,35 +16,36 @@ static size_t data_len;
  * Copy test data to buffer
  */
 static ssize_t
-read(struct file *file, void *buf, size_t len)
+read(struct file *file, void *buf, size_t len, off_t offset)
 {
-	if (file->f_offset >= data_len)
+	if (offset >= data_len)
 		return 0;
-	const size_t r = MIN(data_len - file->f_offset, len);
-	memcpy(buf, data + file->f_offset, r);
-	file->f_offset += r;
+	const size_t r = MIN(data_len - offset, len);
+	memcpy(buf, data + offset, r);
 	return r;
 }
 
 static ssize_t
-read_iov(struct file *file, const struct iovec *iov, size_t count)
+read_iov(struct file *file, const struct iovec *iov, size_t count,
+    off_t offset)
 {
-	return for_each_iov(file, iov, count, read);
+	return for_each_iov(file, iov, count, offset, read);
 }
 
 /*
  * Ignore data written to this driver
  */
 static ssize_t
-write(struct file *file, void *buf, size_t len)
+write(struct file *file, void *buf, size_t len, off_t offset)
 {
 	return len;
 }
 
 static ssize_t
-write_iov(struct file *file, const struct iovec *iov, size_t count)
+write_iov(struct file *file, const struct iovec *iov, size_t count,
+    off_t offset)
 {
-	return for_each_iov(file, iov, count, write);
+	return for_each_iov(file, iov, count, offset, write);
 }
 
 /*
