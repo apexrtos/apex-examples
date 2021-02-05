fbc1e2544f6bf7218fcd7d75c51fb3c7f9b05fa1
Patrick Oppenlander <patrick.oppenlander@gmail.com>
2021-02-05 11:55:47 +1100

    clang support
    * Update busybox to 1.32.0 which contains critical fixes for clang
    * Update busybox build
    * Update shell/config to separate CFLAGS & LDFLAGS and include compiler
      specific flags
    * Update toybox build

--- busybox/busybox.mk
+++ busybox/busybox.mk
@@ -5,24 +5,41 @@
 TYPE := custom
 TARGET := busybox
 
-busybox_CFLAGS := $(CFLAGS) $(CONFIG_USER_CFLAGS)
+# This hackery is required to build a static pie busybox binary.
+# LDFLAGS is supposed to be extra flags for the compiler when invoking the
+# linker but Busybox passes LDFLAGS to $(LD) rather than to $(CC) so we
+# need to shuffle some flags around.
+busybox_LDFLAGS := $(LDFLAGS) $(LDFLAGS_$(COMPILER)) $(CONFIG_USER_LDFLAGS)
+busybox_CFLAGS := $(CFLAGS) $(CFLAGS_$(COMPILER)) $(CONFIG_USER_CFLAGS) \
+		  $(filter -static,$(busybox_LDFLAGS)) \
+		  $(filter -pie,$(busybox_LDFLAGS)) \
+		  $(filter -static-pie,$(busybox_LDFLAGS)) \
+		  -Wno-ignored-optimization-argument \
+		  -Wno-unused-command-line-argument \
+		  -Wno-string-plus-int \
+		  -Wno-format-security \
+		  -Wno-unused-function
+busybox_LDFLAGS := $(filter-out -static,$(busybox_LDFLAGS))
+busybox_LDFLAGS := $(filter-out -pie,$(busybox_LDFLAGS))
+busybox_LDFLAGS := $(filter-out -static-pie,$(busybox_LDFLAGS))
 
-busybox/.config: $(CONFIG_SRCDIR)/busybox/config conf/config.mk| $(CURDIR)/busybox/
+busybox/.config: $(CONFIG_SRCDIR)/busybox/config conf/config.mk | $(CURDIR)/busybox/
 	cp $< $@
 ifeq ($(origin CONFIG_MMU),undefined)
 	echo "CONFIG_NOMMU=y" >> $@
 else
 	echo "# CONFIG_NOMMU is not set" >> $@
 endif
-	echo "CONFIG_CROSS_COMPILER_PREFIX=\"$(CROSS_COMPILE)\"" >> $@
+	echo "CONFIG_CROSS_COMPILER_PREFIX=\"$(CONFIG_CROSS_COMPILE)\"" >> $@
 	echo "CONFIG_EXTRA_CFLAGS=\"$(busybox_CFLAGS)\"" >> $@
+	echo "CONFIG_EXTRA_LDFLAGS=\"$(busybox_LDFLAGS)\"" >> $@
 
-busybox/.copied:
+busybox/.copied: | $(CURDIR)/busybox/
 	cp -rs $(CONFIG_SRCDIR)/busybox/src/* busybox
 	touch $@
 
 busybox/busybox: busybox/.config busybox/.copied
-	MAKEFLAGS="$(_MFLAGS)" $(MAKE) -C busybox LD=$(CROSS_COMPILE)ld AR=$(CROSS_COMPILE)gcc-ar $(SHUTUP_IF_SILENT)
+	MAKEFLAGS="$(_MFLAGS)" $(MAKE) CC=$(CONFIG_CROSS_COMPILE)cc -C busybox $(SHUTUP_IF_SILENT)
 ifneq ($(CONFIG_SSTRIP),)
 	sstrip $@
 endif
--- toybox/toybox.mk
+++ toybox/toybox.mk
@@ -5,7 +5,8 @@
 TYPE := custom
 TARGET := toybox
 
-toybox_CFLAGS := $(CFLAGS) $(CONFIG_USER_CFLAGS)
+toybox_CFLAGS := $(CFLAGS) $(CONFIG_USER_CFLAGS) $(CFLAGS_$(COMPILER))
+toybox_LDFLAGS := $(LDFLAGS) $(CONFIG_USER_LDFLAGS) $(LDFLAGS_$(COMPILER))
 
 toybox/.config: $(CONFIG_SRCDIR)/toybox/config conf/config.mk | $(CURDIR)/toybox/
 	cp $< $@
@@ -22,7 +23,8 @@ toybox/.copied:
 toybox/toybox: toybox/.config toybox/.copied
 	$(MAKE) -C toybox \
 		CFLAGS="$(toybox_CFLAGS) -Wundef -Wno-char-subscripts" \
-		CROSS_COMPILE="$(CROSS_COMPILE)" $(SHUTUP_IF_SILENT)
+		LDFLAGS="$(toybox_LDFLAGS)" \
+		CROSS_COMPILE="$(CONFIG_CROSS_COMPILE)" $(SHUTUP_IF_SILENT)
 	chmod 755 $@
 ifneq ($(CONFIG_SSTRIP),)
 	sstrip $@
